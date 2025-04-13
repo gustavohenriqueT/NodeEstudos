e168caff19c5099ffa3254895f68162e9e26dcca
gustavohenriqueT <gustavo.h.silva@sou.unifeob.edu.br>
2025-04-13 02:46:13 -0300

    funcao concat

---
+++ array/concat.js
@@ -0,0 +1,7 @@
+const filhos = ['Pedro', 'Paulo']
+const filhas = ['Maria', 'Ana']
+
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1,2], [3,4], 5, [[6,7]]))
