266348e99c59e91d6ae9ec4e3efecc472d79b71e
gustavohenriqueT <gustavo.h.silva@sou.unifeob.edu.br>
2025-07-05 04:57:05 -0300

    Promise challenge

---
+++ esnext/desafioPromise.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+const caminho = path.join(__dirname, 'dados.txt')
+
+function lerArq(caminho) {
+    return new Promise(resolve => {
+        fs.readFile(caminho, function (_, conteudo) {
+            resolve(conteudo.toString())
+        })
+        console.log('Depois de ler')
+    })
+}
+
+lerArq(caminho).then(conteudo => conteudo.split('\n')).then(linhas => linhas.join(',')).then(conteudo => `O valor final é: ${conteudo}`).then(console.log)
